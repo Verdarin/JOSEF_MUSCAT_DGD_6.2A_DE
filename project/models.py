762e39576ee408c647f32adede61236c9edd5586
ARIN <josefcarlmuscat0@gmail.com>
2025-03-18 19:54:02 +0100

    Updated Models.py
    Updated PlayerScore class to add constraints to help against SQL injection attacks by making ... a required field, the min and max length is set between 1 and 50. and allow only letters and numbers using regex, and player score needs to be greater or equal to 0

--- project/models.py
+++ project/models.py
@@ -1,6 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
-# Create a Pydantic model for the PlayerScore
+# Define the model for the PlayerScore
 class PlayerScore(BaseModel):
-    player_name: str
-    score: int
+    
+    #... indicates that the field is required
+    # Field() is used to define additional constraints for the field
+    # min_length=1 and max_length=50 are used to define the minimum and maximum length of the player_name
+    # regex=r'^[A-Za-z0-9\s]+$' is used to define a regular expression pattern for the player_name
+    # ge=0 is used to define that the score must be greater than or equal to 0
+    player_name: str = Field(..., min_length=1, max_length=50, regex=r'^[A-Za-z0-9\s]+$')
+    score: int = Field(..., ge=0)
